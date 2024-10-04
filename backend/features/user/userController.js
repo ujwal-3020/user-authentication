6d4ed55ee689049ae32a6aa368c8f77ed4e3a531
ujwal-3020 <ujwal.gupta@argusoft.in>
2024-10-04 11:35:16 +0530

    added new library

--- backend/features/user/userController.js
+++ backend/features/user/userController.js
@@ -27,6 +27,7 @@ const UserController = {
 
       // res.cookie("token", res.token, {
       //   httpOnly: true,
+      //   secure: true,
       //   maxAge: 30 * 24 * 60 * 60 * 1000,
       // });
 
