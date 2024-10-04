264d68e32bf0a8e34e8013c6b68c0e873a7e2e82
ujwal-3020 <ujwal.gupta@argusoft.in>
2024-10-04 09:45:49 +0530

    profile button

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,14 +1,15 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Welcome to the Dashboard</v-toolbar-title>
+      <v-toolbar-title>Food Delivery</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn @click="logout" color="white" variant="text"> Profile </v-btn>
       <v-btn @click="logout" color="white" variant="text"> Logout </v-btn>
     </v-app-bar>
 
     <v-main>
       <v-container class="mt-5">
-        <h1>Hello, {{ user.username }}!</h1>
+        <h1>Welcome to the Dashboard, {{ user.username }}!</h1>
       </v-container>
     </v-main>
   </v-app>
